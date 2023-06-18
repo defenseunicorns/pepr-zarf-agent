5dfb7f695fe8e9f0883e93546166585d8282bda2
Case Wylie <cmwylie19@defenseunicorns.com>
2023-06-17 20:22:19 -0400

    scaffold
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- capabilities/secrets/initSecrets.ts
+++ capabilities/secrets/initSecrets.ts
@@ -4,14 +4,16 @@ import { V1Secret } from "@kubernetes/client-node";
 export class InitSecrets {
   k8sApi: K8sAPI;
 
-  zarfSateSecretName = "zarf-state";
+  zarfStateSecretName = "zarf-state";
   zarfStateSecretNamespace = "zarf";
+  zarfStateSecretKeys = ["registryInfo"]
   privateRegistrySecretName = "private-registry";
   privateRegistrySecretNamespace = "zarf";
+  privateRegistrySecretKeys = [".dockerconfigjson"];
 
-  authServiceNamespace = "authservice";
-  authServiceSecretName = "authservice";
-  authServiceConfigFileName = "config.json";
+  // authServiceNamespace = "authservice";
+  // authServiceSecretName = "authservice";
+  // authServiceConfigFileName = "config.json";
 
   constructor(k8sApi: K8sAPI) {
     this.k8sApi = k8sApi;
@@ -27,6 +29,25 @@ export class InitSecrets {
     return Buffer.from(secret.data[key], "base64").toString("utf-8");
   }
 
+  async getZarfStateSecret(): Promise<ZarfStateSecret> {
+    const secretData = await this.k8sApi.getSecretValues(
+      this.zarfStateSecretName,
+      this.zarfStateSecretNamespace,
+      this.zarfStateSecretKeys
+    );
+
+    return new this.zarfStateSecretKeys()
+  }
+
+  async getZarfPrivateRegistrySecret(): Promise<PrivateRegistrySecret> {
+    const secretData = await this.k8sApi.getSecretValues(
+      this.privateRegistrySecretName,
+      this.privateRegistrySecretNamespace,
+      this.privateRegistrySecretKeys
+    );
+
+    return new this.privateRegistrySecret()
+  }
   async getAuthServiceSecret(): Promise<AuthserviceConfig> {
     const secretData = await this.k8sApi.getSecretValues(
       this.authServiceSecretName,
@@ -67,4 +88,74 @@ export class InitSecrets {
   //     { [this.authServiceConfigFileName]: JSON.stringify(authserviceConfig) }
   //   );
   // }
+}
+
+export class PrivateRegistrySecret {
+  chains: FilterChain[];
+  listen_address: string;
+  listen_port: number;
+  log_level: string;
+  threads: number;
+  trigger_rules?: TriggerRule[];
+  default_oidc_config?: OIDCConfig;
+  allow_unmatched_requests?: boolean;
+
+  constructor(json: any) {
+    this.chains = json.chains.map((chain: any) => new FilterChain(chain));
+    this.listen_address = json.listen_address;
+    this.listen_port = json.listen_port;
+    this.log_level = json.log_level;
+    this.threads = json.threads;
+
+    if (json.trigger_rules !== undefined) {
+      this.trigger_rules = json.trigger_rules.map(
+        (rule: any) => new TriggerRule(rule)
+      );
+    }
+
+    if (json.default_oidc_config !== undefined) {
+      this.default_oidc_config = new OIDCConfig(json.default_oidc_config);
+    }
+
+    if (json.allow_unmatched_requests !== undefined) {
+      this.allow_unmatched_requests = json.allow_unmatched_requests;
+    }
+  }
+
+  static createSingleChain(input: ChainInput): FilterChain {
+    const oidcConfig = new OIDCConfig({
+      callback_uri: input.redirect_uri,
+      client_id: input.id,
+      client_secret: input.secret,
+      cookie_name_prefix: input.name,
+    });
+
+    const filter = new Filter({
+      oidc_override: oidcConfig,
+    });
+
+    const matchMe = new Match({
+      header: ":authority",
+      equality: input.hostname,
+    });
+
+    return new FilterChain({
+      name: input.name,
+      match: matchMe,
+      filters: [filter],
+    });
+  }
+
+  toObject(): Record<string, any> {
+    return {
+      chains: this.chains.map(chain => chain.toObject()),
+      listen_address: this.listen_address,
+      listen_port: this.listen_port,
+      log_level: this.log_level,
+      threads: this.threads,
+      trigger_rules: this.trigger_rules.map(rule => rule.toObject()),
+      default_oidc_config: this.default_oidc_config?.toObject(),
+      allow_unmatched_requests: this.allow_unmatched_requests,
+    };
+  }
 }
