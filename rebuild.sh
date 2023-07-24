79b162d5451de7387984707e3be1282847f81e36
Case Wylie <cmwylie19@defenseunicorns.com>
2023-07-24 17:21:21 -0400

    clean
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kind delete clusters --all;
-docker image prune -a -f;
-kind create cluster
-pepr build;kubectl create -f dist;
-sleep 35;
-kubectl wait --for=condition=Ready pod -l app -n pepr-system --timeout=180s;
-kubectl logs deploy/$(kubectl get deployments --output=jsonpath='{.items[0].metadata.name}' -n pepr-system) -f -n pepr-system
